9571c22e63e734a9967f9d23caef267c300d6318
Clara Rodriguez <clara.rodriguez.a@hotmail.com>
2024-10-14 11:50:55 -0300

    Agregando sentry y toda la cnonfiguración necesaria

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const mongoose = require("mongoose");
 require("dotenv").config();
@@ -8,12 +7,37 @@ const userRoute = require("./routes/user");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+
 const app = express();
+
+// Initialize Sentry
+Sentry.init({
+    dsn: process.env.SENTRY_DSN, // Coloca tu DSN aquí
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+
+
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=> console.log("DB connection successfull"))
 .catch((error)=>{
-    console.log(error);
+    Sentry.captureException(error);
+});
+
+app.get('/debug-sentry', function mainHandler(req, res) {
+    throw new Error('My first Sentry error!');
 });
 
 app.use(express.json());
@@ -23,11 +47,14 @@ app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 
-server = app.listen(process.env.PORT, () => {
-    console.log(`App listening on port ${process.env.PORT || 3000}`);
+// The error handler must be before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+//const port = process.env.PORT || 3000;
+server = app.listen(process.env.PORT || 3000, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
 });
 
 module.exports = server;
 module.exports = app;
-
-//prueba
