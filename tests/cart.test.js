fe6351b0463e9ff556c5c2ef2cc71dde7a1156f0
Clara Rodriguez <clara.rodriguez.a@hotmail.com>
2024-10-14 12:15:46 -0300

    nuevos cambios en test

--- tests/cart.test.js
+++ tests/cart.test.js
@@ -5,15 +5,24 @@ const { MongoMemoryServer } = require('mongodb-memory-server');
 const app = require('../index'); 
 
 let server;
+let timeoutVariable, intervalVariable;
 
 beforeAll(() => {
   server = app.listen();
 });
 
 afterAll(() => {
-  clearTimeout(timeoutVariable);
-  clearInterval(intervalVariable);
-  server.close();
+  // Cierra el servidor y otros manejadores abiertos
+  if (server) server.close();
+
+  // Si tienes Sentry u otras conexiones, asegúrate de cerrarlas también
+  if (typeof Sentry !== 'undefined') {
+    Sentry.close();
+  }
+
+  // Si tienes intervalos o timeouts
+  if (timeoutVariable) clearTimeout(timeoutVariable);
+  if (intervalVariable) clearInterval(intervalVariable);
 });
 
 beforeAll(async () => {
